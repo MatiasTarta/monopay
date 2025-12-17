0015020e2c30137b83c80a05c24eaf006a4abe7b
MatiasTarta <matias.tartaglia@est.fi.uncoma.edu.ar>
2025-12-17 11:51:24 -0300

    conectando render

--- services/socket.ts
+++ services/socket.ts
@@ -1,17 +1,14 @@
 import { io } from "socket.io-client";
-
-const IP = "192.168.0.10"; 
-const PORT = 3000;
-
-const API_URL = `http://${IP}:${PORT}`;
+const API_URL = "https://monopay-1lxl.onrender.com"; 
+// -------------------
 
 console.log("Configurando socket hacia:", API_URL);
 
 export const socket = io(API_URL, {
-  autoConnect: false,
-  transports: ["websocket"], 
+  autoConnect: false, 
+  transports: ["websocket"],
 });
 
-// Listeners para depuración
-socket.on("connect", () => console.log("✅ Conectado al servidor!"));
+// Listeners para depuración (déjalos, son muy útiles)
+socket.on("connect", () => console.log("✅ Conectado al servidor Render!"));
 socket.on("connect_error", (err) => console.log("❌ Error conexión:", err.message));
